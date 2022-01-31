9a48f39af979b612cfacd384721d13007784743f
MAC <mac@Himself.local>
2022-01-31 03:33:40 +0100

    Done with designing

--- user/index.php
+++ user/index.php
@@ -12,74 +12,84 @@
 <body>
     <div class="container">
         <div class="first-view">
-            <img src="../src/img/skill.png" alt="">
-
-            <header>SimpleThings</header>
-            <main>
-                Hi There,<br>
-                It's <span class="simple">Simplethings</span> <br><br>
-                Take your time checking us out!
-            </main>
+            <div>
+                <header>SimpBuild</header>
+                <main>
+                    Hi There,<br>
+                    It's <span class="simple">SimpBuild</span> <br><br>
+                    Take your time checking us out!
+                </main>
+            </div>
+            
         </div>
 
         <article>
             <div>
                 <section class="box">
-                    <h3>What We Do</h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
-                    </p>
+                    <img src="../src/shapes/splash.png" alt="">
+                    <div>
+                        <h3>What We Do</h3>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
+                        </p>
+                    </div>
                 </section>
 
                 <section class="box">
-                    <h3>What We Do</h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
-                    </p>
+                    <img src="../src/shapes/splash-1.png" alt="">
+                    <div>
+                        <h3>What We Do</h3>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
+                        </p>
+                    </div>
                 </section>
 
                 <section class="box">
-                    <h3>What We Do</h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
-                    </p>
+                    <img src="../src/shapes/splash-2.png" alt="">
+                    <div>
+                        <h3>What We Do</h3>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatem fugit deserunt quibusdam eveniet, necessitatibus error quia, iusto quaerat sequi, tenetur nesciunt eligendi id laudantium consequatur neque minima corporis odio.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda ducimus, autem sapiente minus aperiam provident ullam explicabo facilis suscipit atque debitis maxime, veritatis nemo fugiat excepturi vero labore. Perspiciatis.
+                        </p>
+                    </div>
                 </section>
             </div>
 
             <section class="listing">
                 <ul>
                     <li>
-                        <img src="" alt="">
+                        <img src="../src/png/modern-art.png" alt="">
                         <h4>Modern & Clean Design</h4>
                         <span>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi reiciendis, fuga blanditiis distinctio in at temporibus, sed nostrum amet similique
                         </span>
                     </li>
                     <li>
-                        <img src="" alt="">
+                        <img src="../src/png/responsive-website.png" alt="">
                         <h4>Responsive Design</h4>
                         <span>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi reiciendis, fuga blanditiis distinctio in at temporibus, sed nostrum amet similique
                         </span>
                     </li>
                     <li>
-                        <img src="" alt="">
+                        <img src="../src/png/vulnerable-individuals.png" alt="">
                         <h4>Strong Security</h4>
                         <span>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi reiciendis, fuga blanditiis distinctio in at temporibus, sed nostrum amet similique
                         </span>
                     </li>
                     <li>
-                        <img src="" alt="">
+                        <img src="../src/png/process.png" alt="">
                         <h4>Consistency</h4>
                         <span>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi reiciendis, fuga blanditiis distinctio in at temporibus, sed nostrum amet similique
@@ -88,7 +98,111 @@
                 </ul>
             </section>
         </article>
+
+        <div class="projects">
+            <div class="project-head">
+                <h4>Website</h4>
+                <h4>Local Projects</h4>
+            </div>
+            <div class="website project-cont">
+                <div class="web-desc">
+                    <h5><a href="https:\\macrounique.co" target="_blank">Macrounique</a></h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus assumenda sequi molestias et voluptatibus earum voluptate aliquam soluta at, nostrum nesciunt modi dolore quia quos fuga? Iste, magni cum?
+                    </p>
+                    <br>             
+                    <img src="../src/files/macro WEB logoAsset 2.png" alt="">
+
+                    <h5><a href="https:\\quandaryvisions.com" target="_blank">Quandaryvisions</a></h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde corporis cupiditate corrupti facere sint, beatae recusandae rerum accusamus. Aut voluptas dolorum eaque deserunt iure necessitatibus molestiae fugit expedita enim.
+                    </p>
+                    <br>
+                    <img src="../src/files/quandary_logo.png" alt="">
+                </div>
+            </div>
+            <div class="local project-cont">
+                <div class="local-desc">
+                    <h5><a href="https://github.com/Valentine112/Todo-list" target="_blank">A simple todo list</a></h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde corporis cupiditate corrupti facere sint, beatae recusandae rerum accusamus. Aut voluptas dolorum eaque deserunt iure necessitatibus molestiae fugit expedita enim.
+                        <br><br>
+                        <q><small>Raw code</small></q>
+                    </p>
+                    <br>
+                    
+                    <h5><a href="https://github.com/Valentine112/Terminal-Social-Media" target="_blank">A terminal social media</a></h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde corporis cupiditate corrupti facere sint, beatae recusandae rerum accusamus. Aut voluptas dolorum eaque deserunt iure necessitatibus molestiae fugit expedita enim.
+                        <br><br>
+                        <q><small>Raw code</small></q>
+                    </p>
+
+                    <h5><a href="https://github.com/Valentine112/Upload-Directory-Images" target="_blank">A script capable of uploading all images in a directory</a></h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde corporis cupiditate corrupti facere sint, beatae recusandae rerum accusamus. Aut voluptas dolorum eaque deserunt iure necessitatibus molestiae fugit expedita enim.
+                        <br><br>
+                        <q><small>Raw code</small></q>
+                    </p>
+                </div>
+            </div>
+        </div>
         
+        <div class="about-me headings">
+            <h1>About me</h1>
+            <div class="my-header">
+                <img src="../src/files/me.jpeg" alt="">
+                <h3>Ngene Valentine <br><small>Fullstack web developer</small></h3>
+            </div>
+            <div class="my-desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error sunt, aliquid impedit possimus rerum obcaecati delectus dolore minus nostrum ipsa perferendis excepturi maiores repellat ut. Ea exercitationem harum similique!
+
+                <br><br>
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci asperiores eveniet, assumenda ipsum totam doloribus consequuntur ratione officia nisi laborum, atque necessitatibus neque temporibus quis eos ab nostrum corporis delectus.
+            </div>
+        </div>
+
+        <div class="contact-me headings">
+            <h1>Contact me</h1>
+            <div>
+                <form action="" method="post">
+                    <input type="text" class="form" placeholder="Full name" required name="" id="">
+                    <input type="email" class="form" placeholder="Email address" required name="" id="">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="message . . ." required></textarea>
+                    <div class="suggest">
+                        <small>suggestions</small><br>
+                        <q>Can you build a website for me?</q>
+                        <q>Do you work remotely?</q>
+                        <q>How long would it take to build a website for me?</q>
+                    </div>
+                    <button>Send</button>
+                </form>
+            </div>
+        </div>
+        <footer>
+            <h2>SimpBuild</h2>
+            <div>
+                Location: Nigeria, Imo state, Owerri west
+                <br>
+                <span>
+                    <img src="../src/social/ig.svg" alt="Instagram"> - <a href="https:\\instagram.com\val_clouds" target="_blank">Val_clouds</a> &ensp;
+                </span>
+
+                <span>
+                    <img src="../src/social/fb.svg" alt="Facebook"> - <a href="https:\\facebook.com/ngene.valentine.54" target="_blank">Ngene Valentine</a> &ensp;
+                </span>
+
+                <span>
+                    <img src="../src/social/tw.svg" alt=""> - <a href="https:\\twitter.com/Himself_xx" target="_blank">Himself_xx</a> &ensp;
+                </span>
+
+                <span>
+                    <img src="../src/social/wa.svg" alt="WhatsApp"> - <a href="https://wa.me/2347018208311" target="_blank">Valentine</a> &ensp;
+                </span>
+                <br>
+                <div class="copy">Copyright 2022</div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
